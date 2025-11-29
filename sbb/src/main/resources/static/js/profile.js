5fb400889002a27eee4f8f7cd2c594907182ea3e
HwOdust <todka05@gmail.com>
2025-11-30 03:17:50 +0900

    프로필 프론트 수정

--- sbb/src/main/resources/static/js/profile.js
+++ sbb/src/main/resources/static/js/profile.js
@@ -5,15 +5,22 @@ tabButtons.forEach(btn => {
     btn.addEventListener('click', () => {
         const target = btn.dataset.tab;
 
+        // 모든 버튼 초기화
         tabButtons.forEach(b => {
-            b.classList.remove('border-blue-600', 'font-medium');
+            b.classList.remove('border-blue-600', 'border-b-2', 'text-black');
             b.classList.add('text-gray-600');
         });
 
-        btn.classList.add('border-blue-600', 'font-medium');
+        // 선택된 버튼 활성화
+        btn.classList.add('border-blue-600', 'border-b-2', 'text-black');
         btn.classList.remove('text-gray-600');
 
+        // 탭 내용 전부 숨기기
         tabContents.forEach(c => c.classList.add('hidden'));
+
+        // 클릭된 탭만 보이기
         document.getElementById(`tab-${target}`).classList.remove('hidden');
     });
 });
+
+document.querySelector('.tab-btn[data-tab="activity"]').click();
